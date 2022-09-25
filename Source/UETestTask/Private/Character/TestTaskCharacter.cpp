4cfa7e0d9b356efc3ff12340657481df6ac4c57d
DESKTOP-7NJKNFV\Ilya <ilyavoina72@gmail.com>
2022-09-26 01:52:05 +0300

    Added basic character movement

--- Source/UETestTask/Private/Character/TestTaskCharacter.cpp
+++ Source/UETestTask/Private/Character/TestTaskCharacter.cpp
@@ -21,7 +21,48 @@ ATestTaskCharacter::ATestTaskCharacter()
 void ATestTaskCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void ATestTaskCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &ACharacter::Jump);
 	
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ThisClass::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ThisClass::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ThisClass::Turn);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &ThisClass::LookUp);
+}
+
+void ATestTaskCharacter::MoveForward(const float Value)
+{
+	if(Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ATestTaskCharacter::MoveRight(const float Value)
+{
+	if(Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ATestTaskCharacter::Turn(const float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ATestTaskCharacter::LookUp(const float Value)
+{
+	AddControllerPitchInput(Value);
 }
 
 void ATestTaskCharacter::Tick(float DeltaTime)
@@ -29,10 +70,3 @@ void ATestTaskCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
-void ATestTaskCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
